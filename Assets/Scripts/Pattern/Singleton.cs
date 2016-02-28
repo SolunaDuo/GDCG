35fd7fa0fdca4afc8b8406451150754eee8568bd
NaturaAurum <natura0321@gmail.com>
2016-02-28 23:55:25 +0900

    First time

---
+++ Assets/Scripts/Pattern/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour, new()
+{
+    protected static T instance;
+
+    public void CreateInstance()
+    {
+        if ( instance == null )
+        {
+            GameObject ins = new GameObject( typeof( T ).Name );
+        }
+    }
+
+    public static T get()
+    {
+        if ( instance == null )
+        {
+            instance = FindObjectOfType<T>();
+            if ( instance == null )
+            {
+                GameObject ins = new GameObject( typeof( T ).Name );
+            }
+        }
+
+        return instance;
+    }
+}
