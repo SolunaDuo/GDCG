d97d74d44c1de2f4407f38dfd35a96e238706d4c
SHJ <junwe99@gmail.com>
2016-03-27 00:01:26 +0900

    0327

---
+++ Assets/Scripts/Global/cSceneManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+제작자  : 서형준
+만든 날 : 2016-03-23
+기능    : 씬 관리 스크립트
+*/
+
+public class cSceneManager : MonoBehaviour {
+    private static cSceneManager Instance = null;
+
+
+    public Texture     FadeImg;
+
+    private string      szSceneName = "";
+    private int         nSceneLevel = -1;
+
+    private float       fFadeTime;
+    private int         nFadeDir = 0;
+    private float       fAlpha;
+
+    private int         drawDepth= -1000;
+
+
+    public static cSceneManager instance
+    {
+        get
+        {
+            if (Instance == null)
+            {
+                Instance = GameObject.Find("SCENEMGR").GetComponent< cSceneManager>();
+            }
+            return Instance;
+        }
+    }
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+
+    void OnGUI()
+    {
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, fAlpha);
+
+        GUI.depth = drawDepth;
+
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), FadeImg);
+    }
+
+    IEnumerator StartFade()
+    {
+        yield return new WaitForEndOfFrame();
+        float elasptime = 0.0f;
+
+        while(elasptime <= fFadeTime)
+        {
+            elasptime += Time.deltaTime;
+
+            if(nFadeDir == 0)
+                fAlpha = nFadeDir + (elasptime / fFadeTime);
+            else
+                fAlpha = nFadeDir - (elasptime / fFadeTime);
+            yield return new WaitForEndOfFrame();
+        }
+        if (!szSceneName.Equals(""))
+            Application.LoadLevel(szSceneName);
+        if(nSceneLevel >= 0 )
+            Application.LoadLevel(nSceneLevel);
+
+        fAlpha = 0.0f;
+
+    }
+
+    public void LoadLevel(string name,float fadetime)
+    {
+        fFadeTime = fadetime;
+
+        szSceneName = name;
+        nSceneLevel = -1;
+
+        StartCoroutine( StartFade() );
+    }
+
+    public void LoadLevel(int level, float fadetime)
+    {
+        fFadeTime = fadetime;
+
+        szSceneName = "";
+        nSceneLevel = level;
+
+        StartCoroutine(StartFade());
+    }
+}
---
+++ Assets/Scripts/LTEXT.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+/*
+제작자  : 서형준
+만든 날 : 2016-03-23
+기능    : 텍스트를 읽어오는 스크립트
+*/
+
+
+public class LTEXT : MonoBehaviour {
+
+    private static List<string> ltext = null;
+
+    private static bool bInit = false;
+
+    private static void Init(string szFile)
+    {
+        TextAsset texAsst = (TextAsset)Resources.Load(szFile);
+
+        if(ltext == null)
+        {
+            ltext = new List<string>();
+        }
+        else
+        {
+            ltext.Clear();
+            bInit = false;
+        }
+
+        if (texAsst != null)
+        {
+            char[] charsplit = new char[] {'\r','\n'};
+            string[] token1;
+            token1 = texAsst.text.Split(charsplit, System.StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < token1.Length; ++i)
+            {
+                char[] charsplit2 = new char[] { ',', '\t' };
+                string[] token2 = token1[i].Split(charsplit2, System.StringSplitOptions.RemoveEmptyEntries);
+
+                if (token2 != null)
+                {
+                    ltext.Add(token2[1]);
+                }
+                else
+                {
+                    ltext.Add("null");
+                }
+            }   
+        }
+
+        bInit = true;
+    }
+
+    public static void LoadText(string szFile)
+    {
+        Init(szFile);
+    }
+
+    public static string Get(int i)
+    {
+        if (!bInit) return "null";
+
+        if(ltext.Count < i)
+        {
+            return "null";
+        }
+
+        return ltext[i];
+    }
+}
