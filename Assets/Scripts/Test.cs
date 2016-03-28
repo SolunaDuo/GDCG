4d9e6721cb04561d766f0a91618b9e4727b1301e
김태우 <natura0321@gmail.com>
2016-03-28 16:20:10 +0900

    안올린 것들

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : BehaviourBase {
+
+    public override void Awake()
+    {
+        base.Awake();
+        Debug.Log( "Awake" );
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        Debug.Log( "Start" );
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        Debug.Log( "Update" );
+    }
+
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        Debug.Log( "FixedUpdate" );
+    }
+
+    public override void LateUpdate()
+    {
+        base.LateUpdate();
+        Debug.Log( "LateUpdate" );
+    }
+}
