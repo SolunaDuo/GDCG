e18a430f9f1a5a375138df2ecb454948b46ca04d
SHJ <junwe99@gmail.com>
2016-04-01 00:50:58 +0900

    0401)Panel_Employee 추가(만드는중)

---
+++ Assets/Scripts/EmployeeScene/Panel_Employee.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+제작자  : 서형준
+만든 날 : 2016-03-31
+기능    : 직원 관리 팝업 패널 스크립트.
+*/
+
+struct MyEmployee
+{
+    public bool isActive;
+    public int     nNum; // 몇번째 인지 체크
+
+    public ST_EMPLOYEE_INFO StInfo;
+
+
+};
+
+public class Panel_Employee : MonoBehaviour {
+    public static Panel_Employee instance = null;
+
+    List<MyEmployee> listMyEmployee;  // 직원 리스트
+    public GameObject pfEmpItem;
+
+    public GameObject panel_grid;   // 그리드 패널
+    
+
+    void Awake()
+    {
+        instance = this;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SetMyEmployee(ST_EMPLOYEE_INFO temp)
+    {
+        MyEmployee Metemp = new MyEmployee();
+        // Employee Setting
+        Metemp.isActive = true;
+        Metemp.nNum = listMyEmployee.Count;
+        Metemp.StInfo = new ST_EMPLOYEE_INFO(temp);
+        // 프리팹 만들어줌
+        GameObject tempobj = (GameObject)Instantiate(pfEmpItem);
+        tempobj.transform.parent = panel_grid.transform;
+
+        //패널 크기 세팅 필요
+
+        listMyEmployee.Add(Metemp);
+    }
+}
--- Assets/Scripts/Global/GlobalInfo.cs
+++ Assets/Scripts/Global/GlobalInfo.cs
@@ -18,7 +18,7 @@ public class ST_GAME_INFO
     public int Rank;       // 게임 등급
     public int Taget;      // 타겟층
 
-    public ST_GAME_INFO(float st1, float st2, float st3, int money,int rank,int taget)
+    public ST_GAME_INFO(float st1, float st2, float st3, int money, int rank, int taget)
     {
         State1 = st1;
         State2 = st2;
@@ -27,5 +27,38 @@ public class ST_GAME_INFO
         Money = money;
         Rank = rank;
         Taget = taget;
+    }
+}
+
+[Serializable]
+public class ST_EMPLOYEE_INFO
+{
+    public float State1;   // 스테이스 4개
+    public float State2;
+    public float State3;
+    public float State4;
+
+    public int Money;      // 연봉
+    public string Name;    // 이름
+
+
+    public ST_EMPLOYEE_INFO(float st1, float st2, float st3, float st4, int money, string name)
+    {
+        State1 = st1;
+        State2 = st2;
+        State3 = st3;
+
+        Money = money;
+        Name = name;
+    }
+
+    public ST_EMPLOYEE_INFO(ST_EMPLOYEE_INFO temp)
+    {
+        State1 = temp.State1;
+        State2 = temp.State2;
+        State3 = temp.State3;
+
+        Money = temp.Money;
+        Name = temp.Name;
     }
 }
