5f46906674b9b555e6d37cf9af55a9811ee76473
NaturaAurum <natura0321@gmail.com>
2016-04-07 23:44:08 +0900

    ObjectManager와 샘플 캐릭터 이미지 추가

---
+++ Assets/Scripts/Manager/ObjectManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class ObjectManager : Singleton<ObjectManager>
+{
+
+    private Dictionary<string, Dictionary<string, GameObject>> objectList = new Dictionary<string, Dictionary<string, GameObject>>();
+    private string currentSceneName;
+
+    void Awake()
+    {
+
+    }
+
+    public void SetCurretScene()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        objectList.Add( sceneName, new Dictionary<string, GameObject>() );
+        currentSceneName = sceneName;
+    }
+
+    public void SetCurrentScene( string sceneName )
+    {
+        objectList.Add( sceneName, new Dictionary<string, GameObject>() );
+        currentSceneName = sceneName;
+    }
+
+    public void Clear( string sceneName )
+    {
+        objectList[ sceneName ].Clear();
+    }
+
+    public void AllClear()
+    {
+        Dictionary<string, Dictionary<string, GameObject>>.Enumerator iter = objectList.GetEnumerator();
+
+        while ( iter.MoveNext() )
+        {
+            iter.Current.Value.Clear();
+        }
+        iter.Dispose();
+    }
+}
