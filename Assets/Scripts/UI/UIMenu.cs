9acac37ea8432ca63471be7d818ebbd9696637b7
김태우 <natura0321@gmail.com>
2016-04-08 17:22:15 +0900

    여러가지 잠깐 해봤

---
+++ Assets/Scripts/UI/UIMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIMenu : MonoBehaviour
+{
+
+    
+
+    // Use this for initialization
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
