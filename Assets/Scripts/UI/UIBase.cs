60b1f95dbca5b06acf3be219bdc635ba577fbcef
NaturaAurum <natura0321@gmail.com>
2016-04-14 22:40:22 +0900

    UIBase Pointer 관련 interface 전부 상속

--- Assets/Scripts/UI/UIBase.cs
+++ Assets/Scripts/UI/UIBase.cs
@@ -4,11 +4,31 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System;
 
-public class UIBase : MonoBehaviour, IPointerClickHandler
+public class UIBase : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
     public virtual void OnPointerClick( PointerEventData eventData )
     {
-        
+        Debug.Log( "Pointer Click" );
+    }
+
+    public virtual void OnPointerDown( PointerEventData eventData )
+    {
+        Debug.Log( "Pointer Down" );
+    }
+
+    public virtual void OnPointerEnter( PointerEventData eventData )
+    {
+        Debug.Log( "Pointer Enter" );
+    }
+
+    public virtual void OnPointerExit( PointerEventData eventData )
+    {
+        Debug.Log( "Pointer Exit" );
+    }
+
+    public virtual void OnPointerUp( PointerEventData eventData )
+    {
+        Debug.Log( "Pointer Up" );
     }
 
     protected void AddListener( Button button, UnityEngine.Events.UnityAction action )
