59552db754fb26a845c07b059deeb41c446fc4be
NaturaAurum <natura0321@gmail.com>
2016-04-15 01:19:23 +0900

    계속 짤막짤막 수정중..

--- Assets/Scripts/UI/UIBase.cs
+++ Assets/Scripts/UI/UIBase.cs
@@ -6,6 +6,23 @@ using System;
 
 public class UIBase : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
+
+    protected struct TEMP_PLATFORMSTRING
+    {
+        public const string PLATFORM_PC = "PC";
+        public const string PLATFORM_ANDROID = "Android";
+        public const string PLATFORM_IOS = "IOS";
+    }
+
+    protected struct TEMP_GENRESTRING
+    {
+        public const string GENRE_PUZZLE = "퍼즐";
+        public const string GENRE_BOARD = "보드";
+        public const string GENRE_ARCADE = "아케이드";
+        public const string GENRE_ROLEPLAYING = "롤플레잉";
+        public const string GENRE_ACTION = "액션";
+    }
+
     public virtual void OnPointerClick( PointerEventData eventData )
     {
         Debug.Log( "Pointer Click" );
